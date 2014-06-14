f679af2fdb0b8c1ca14bf9ac0fb088029325e6bc
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-15 00:37:07 +0400

    Coordinates

---
+++ executables/CoordinatesTests.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+import Test.Framework
+import MutableContainers.Prelude
+import MutableContainers.Coordinates
+import Numeric
+import Data.Char
+import NumericQQ
+
+
+main = htfMain $ htf_thisModulesTests
+
+
+test_1 = do
+  assertEqual expected real
+  where
+    expected = [0, [bin|1010|], [bin|0100|]]
+    real = reverse $ coordinates chunkSize chunkMask depth index where
+      chunkSize = 4
+      chunkMask = [bin|1111|]
+      depth = 3
+      index = [bin|000010100100|]
+    
+
