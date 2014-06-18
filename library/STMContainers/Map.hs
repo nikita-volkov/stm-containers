cb64168d347874efa4d8e895e4bb7da745ed9684
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-18 18:31:24 +0400

    HashTable -> Map

--- library/STMContainers/Map.hs
+++ library/STMContainers/Map.hs
@@ -1,24 +1,24 @@
-module STMContainers.HashTable where
+module STMContainers.Map where
 
 import STMContainers.Prelude
 import qualified STMContainers.HAMT as HAMT
 
 
 -- |
--- A hash array mapped trie, specialized for STM.
-type HashTable k v = HAMT.Node k v
+-- A hash table, based on an STM-specialized hash array mapped trie.
+type Map k v = HAMT.Node k v
 
 -- |
 -- A constraint for keys.
 type IsKey k = (Eq k, Hashable k)
 
-insert :: (IsKey k) => k -> v -> HashTable k v -> STM ()
+insert :: (IsKey k) => k -> v -> Map k v -> STM ()
 insert k v t = HAMT.insert (hash k, k) v (0, t)
 
-delete :: (IsKey k) => k -> HashTable k v -> STM ()
+delete :: (IsKey k) => k -> Map k v -> STM ()
 delete k t = void $ HAMT.delete (hash k, k) (0, t)
 
-lookup :: (IsKey k) => k -> HashTable k v -> STM (Maybe v)
+lookup :: (IsKey k) => k -> Map k v -> STM (Maybe v)
 lookup k t = HAMT.lookup (hash k, k) (0, t)
 
 
