b8fbc0e1e0f37af5cb345a587bdd6fd12cba06dc
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-19 19:23:04 +0400

    Fill up the Visit API

--- library/STMContainers/Visit.hs
+++ library/STMContainers/Visit.hs
@@ -5,6 +5,11 @@ import STMContainers.Prelude
 
 -- |
 -- A general modification function for some match.
+-- 
+-- By processing a 'Maybe' value it produces some value to emit and 
+-- a 'Command' to perform to the match.
+-- 
+-- The interpretation of this function is up to the context APIs.
 type Visit a r = Maybe a -> (r, Command a)
 
 -- |
@@ -13,6 +18,8 @@ type VisitM m a r = Maybe a -> m (r, Command a)
 
 -- |
 -- What to do with the visited value.
+-- 
+-- The interpretation of the commands is up to the context APIs.
 data Command a =
   Keep |
   Remove |
@@ -26,18 +33,42 @@ monadize :: (Monad m) => Visit a r -> VisitM m a r
 monadize = fmap return
 
 
--- * Constructors for standard patterns
+-- * Constructors for common pure patterns
 -------------------------
 
-update :: (a -> a) -> Visit a ()
-update f = maybe ((), Keep) (\a -> ((), Replace (f a)))
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:adjust adjust>@.
+adjust :: (a -> a) -> Visit a ()
+adjust f = maybe ((), Keep) (\a -> ((), Replace (f a)))
 
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:update update>@.
+update :: (a -> Maybe a) -> Visit a ()
+update f = maybe ((), Keep) (\a -> ((), maybe Remove Replace (f a)))
+
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:alter alter>@.
+alter :: (Maybe a -> Maybe a) -> Visit a ()
+alter f = ((),) . maybe Remove Replace . f
+
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:insert insert>@.
 insert :: a -> Visit a ()
 insert a = const ((), Replace a)
 
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:delete delete>@.
 delete :: Visit a ()
 delete = const ((), Remove)
 
+-- |
+-- Reproduces the behaviour of
+-- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:lookup lookup>@.
 lookup :: Visit a (Maybe a)
 lookup r = (r, Keep)
 
@@ -45,7 +76,17 @@ lookup r = (r, Keep)
 -- * Constructors for monadic patterns
 -------------------------
 
-updateM :: (Monad m) => (a -> m a) -> VisitM m a ()
-updateM f = \case
-  Nothing -> return ((), Keep)
-  Just a -> f a >>= return . ((), ) . Replace
+-- |
+-- A monadic version of 'adjust'.
+adjustM :: (Monad m) => (a -> m a) -> VisitM m a ()
+adjustM f = maybe (return ((), Keep)) (liftM (((),) . Replace) . f)
+
+-- |
+-- A monadic version of 'update'.
+updateM :: (Monad m) => (a -> m (Maybe a)) -> VisitM m a ()
+updateM f = maybe (return ((), Keep)) (liftM (((),) . maybe Remove Replace) . f)
+
+-- |
+-- A monadic version of 'alter'.
+alterM :: (Monad m) => (Maybe a -> m (Maybe a)) -> VisitM m a ()
+alterM f = liftM (((),) . maybe Remove Replace) . f
