834ffa92b0b1908fb38726a4fac71e9184646617
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-19 16:39:21 +0400

    Alter updates

--- library/STMContainers/Alter.hs
+++ library/STMContainers/Alter.hs
@@ -2,6 +2,7 @@ module STMContainers.Alter where
 
 import STMContainers.Prelude
 
+
 -- |
 -- A general modification function for some match.
 type Alter a r = Maybe a -> (r, Command a)
@@ -17,3 +18,22 @@ data Command a =
   Remove |
   Replace a
   deriving (Functor)
+
+-- |
+-- Convert a pure 'Alter' into a monadic one.
+monadize :: (Monad m) => Alter a r -> AlterM m a r
+monadize = fmap return
+
+
+-- * Lifters for standard altering patterns
+-------------------------
+
+liftUpdate :: (a -> a) -> Alter a ()
+liftUpdate f = \case
+  Nothing -> ((), Keep)
+  Just a -> ((), Replace (f a))
+
+liftUpdateM :: (Monad m) => (a -> m a) -> AlterM m a ()
+liftUpdateM f = \case
+  Nothing -> return ((), Keep)
+  Just a -> f a >>= return . ((), ) . Replace
