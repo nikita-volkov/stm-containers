f7bd657adceedf7f3c22340f2043a9b51d68f2a1
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-24 17:42:04 +0400

    Update concurrency bench

--- executables/ConcurrentInsertionBench.hs
+++ executables/ConcurrentInsertionBench.hs
@@ -10,20 +10,32 @@ main =
   [
     bgroup "STM Containers"
     [
-      stmContainersBench 1 10000,
-      stmContainersBench 2 5000,
-      stmContainersBench 4 2500,
-      stmContainersBench 8 1250
+      stmContainersBench 1 (max `div` 1),
+      stmContainersBench 2 (max `div` 2),
+      stmContainersBench 4 (max `div` 4),
+      stmContainersBench 8 (max `div` 8),
+      stmContainersBench 16 (max `div` 16),
+      stmContainersBench 32 (max `div` 32),
+      stmContainersBench 64 (max `div` 64),
+      stmContainersBench 128 (max `div` 128),
+      stmContainersBench 256 (max `div` 256)
     ]
     ,
     bgroup "Unordered Containers + TVar"
     [
-      unorderedContainersTVarBench 1 10000,
-      unorderedContainersTVarBench 2 5000,
-      unorderedContainersTVarBench 4 2500,
-      unorderedContainersTVarBench 8 1250
+      unorderedContainersTVarBench 1 (max `div` 1),
+      unorderedContainersTVarBench 2 (max `div` 2),
+      unorderedContainersTVarBench 4 (max `div` 4),
+      unorderedContainersTVarBench 8 (max `div` 8),
+      unorderedContainersTVarBench 16 (max `div` 16),
+      unorderedContainersTVarBench 32 (max `div` 32),
+      unorderedContainersTVarBench 64 (max `div` 64),
+      unorderedContainersTVarBench 128 (max `div` 128),
+      unorderedContainersTVarBench 256 (max `div` 256)
     ]
   ]
+  where
+    max = 6000
 
 stmContainersBench :: Int -> Int -> Benchmark
 stmContainersBench threads rows =
