b17a8fc1ae5eb66b09377b46a9dad9e275cb9be3
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-25 16:10:55 +0400

    Profiling

---
+++ executables/Profiling.hs
@@ -0,0 +1,14 @@
+
+import STMContainers.Prelude
+import qualified STMContainers.Map as STMContainers
+import qualified Control.Concurrent.Async as Async
+
+
+main = do
+  t <- atomically $ STMContainers.new :: IO (STMContainers.Map Int ())
+  void $ flip Async.mapConcurrently [0 .. pred threads] $ \ti -> do
+    forM_ [ti * (rows `div` threads) .. (succ ti) * (rows `div` threads)] $ \ri -> do
+      atomically $ STMContainers.insert ri () t
+  where
+    threads = 4
+    rows = 100000
