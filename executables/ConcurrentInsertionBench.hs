8d9416124f67e8fdda12bb1f9cd45ea55e7f0df0
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-25 12:35:00 +0400

    Correct concurrency bench

--- executables/ConcurrentInsertionBench.hs
+++ executables/ConcurrentInsertionBench.hs
@@ -5,55 +5,43 @@ import qualified Data.HashMap.Strict as UnorderedContainers
 import qualified STMContainers.Map as STMContainers
 import qualified Control.Concurrent.Async as Async
 
+
 main =
   defaultMain
   [
-    bgroup "STM Containers"
-    [
-      stmContainersBench 1 (max `div` 1),
-      stmContainersBench 2 (max `div` 2),
-      stmContainersBench 4 (max `div` 4),
-      stmContainersBench 8 (max `div` 8),
-      stmContainersBench 16 (max `div` 16),
-      stmContainersBench 32 (max `div` 32),
-      stmContainersBench 64 (max `div` 64),
-      stmContainersBench 128 (max `div` 128),
-      stmContainersBench 256 (max `div` 256)
-    ]
-    ,
-    bgroup "Unordered Containers + TVar"
-    [
-      unorderedContainersTVarBench 1 (max `div` 1),
-      unorderedContainersTVarBench 2 (max `div` 2),
-      unorderedContainersTVarBench 4 (max `div` 4),
-      unorderedContainersTVarBench 8 (max `div` 8),
-      unorderedContainersTVarBench 16 (max `div` 16),
-      unorderedContainersTVarBench 32 (max `div` 32),
-      unorderedContainersTVarBench 64 (max `div` 64),
-      unorderedContainersTVarBench 128 (max `div` 128),
-      unorderedContainersTVarBench 256 (max `div` 256)
-    ]
+    threadsRowsGroup 1 (max `div` 1),
+    threadsRowsGroup 3 (max `div` 3),
+    threadsRowsGroup 6 (max `div` 6),
+    threadsRowsGroup 12 (max `div` 12),
+    threadsRowsGroup 24 (max `div` 24)
   ]
   where
-    max = 6000
+    max = 30000
+
+threadsRowsGroup :: Int -> Int -> Benchmark
+threadsRowsGroup threads rows =
+  bgroup 
+  (shows threads . showString "/" . shows rows $ "")
+  [
+    stmContainersBench threads rows,
+    unorderedContainersTVarBench threads rows
+  ]
 
 stmContainersBench :: Int -> Int -> Benchmark
 stmContainersBench threads rows =
-  bench (shows threads . showString "/" . shows rows $ "") $ do
+  bench ("STM Containers") $ do
     t <- atomically $ STMContainers.new :: IO (STMContainers.Map Int ())
-    replicateConcurrently_ threads $ atomically $ do
-      forM_ [0..rows] $ \i -> STMContainers.insert i () t
+    flip Async.mapConcurrently [0 .. pred threads] $ \ti -> do
+      forM_ [rows * ti .. (pred rows) * ti] $ \ri -> do
+        atomically $ STMContainers.insert ri () t
 
 unorderedContainersTVarBench :: Int -> Int -> Benchmark
 unorderedContainersTVarBench threads rows =
-  bench (shows threads . showString "/" . shows rows $ "") $ do
+  bench ("Unordered Containers + TVar") $ do
     t <- newTVarIO UnorderedContainers.empty :: IO (TVar (UnorderedContainers.HashMap Int (TVar ())))
-    replicateConcurrently_ threads $ atomically $ do
-      forM_ [0..rows] $ \i -> do
-        c <- newTVar ()
-        tv <- readTVar t
-        writeTVar t $! UnorderedContainers.insert i c tv
-
-replicateConcurrently_ :: Int -> IO () -> IO ()
-replicateConcurrently_ n io = void $ Async.mapConcurrently (const io) [1 .. n]
-
+    flip Async.mapConcurrently [0 .. pred threads] $ \ti -> do
+      forM_ [rows * ti .. (pred rows) * ti] $ \ri -> 
+        atomically $ do
+          c <- newTVar ()
+          tv <- readTVar t
+          writeTVar t $! UnorderedContainers.insert ri c tv
