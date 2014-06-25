7300cba6fc05a7b9c60b64995a798e879b3c085b
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-25 19:54:59 +0400

    Inline Level

--- library/STMContainers/HAMT/Node/Level.hs
+++ library/STMContainers/HAMT/Node/Level.hs
@@ -8,17 +8,22 @@ import STMContainers.Prelude hiding (mask)
 -- Must be a multiple of the 'step' value.
 type Level = Int
 
+{-# INLINE hashIndex #-}
 hashIndex :: Level -> (Int -> Int)
 hashIndex l i = mask .&. unsafeShiftR i l
 
+{-# INLINE mask #-}
 mask :: Int
 mask = bit step - 1
 
+{-# INLINE step #-}
 step :: Int
 step = 5
 
+{-# INLINE limit #-}
 limit :: Int
 limit = bitSize (undefined :: Int)
 
+{-# INLINE succ #-}
 succ :: Level -> Level
 succ = (+ step)
