51f3a030e5368e53b5bbd3828d799b1684c8c777
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-06-26 04:03:23 +0400

    Update InsertionBench

--- executables/InsertionBench.hs
+++ executables/InsertionBench.hs
@@ -6,6 +6,7 @@ import qualified Data.HashMap.Strict as UnorderedContainers
 import qualified Data.Map as Containers
 import qualified STMContainers.Map as STMContainers
 import qualified Focus
+import qualified System.Random as Random
 
 main =
   defaultMain
@@ -14,33 +15,37 @@ main =
     [
       bench "focus-based" $ 
         do
-          t <- atomically $ STMContainers.new :: IO (STMContainers.Map Int ())
-          forM_ [0..rows] $ \i -> atomically $ STMContainers.focus (Focus.insertM ()) i t
+          t <- atomically $ STMContainers.new :: IO (STMContainers.Map String ())
+          forM_ [0..rows] $ \i -> atomically $ STMContainers.focus (Focus.insertM ()) (iToK i) t
       ,
       bench "specialized" $
         do
-          t <- atomically $ STMContainers.new :: IO (STMContainers.Map Int ())
-          forM_ [0..rows] $ \i -> atomically $ STMContainers.insert i () t
+          t <- atomically $ STMContainers.new :: IO (STMContainers.Map String ())
+          forM_ [0..rows] $ \i -> atomically $ STMContainers.insert (iToK i) () t
     ]
     ,
     bench "Unordered Containers + TVar" $
       do
-        t <- newTVarIO UnorderedContainers.empty :: IO (TVar (UnorderedContainers.HashMap Int (TVar ())))
+        t <- newTVarIO UnorderedContainers.empty :: IO (TVar (UnorderedContainers.HashMap String (TVar ())))
         forM_ [0..rows] $ \i -> atomically $ do
           c <- newTVar ()
           tv <- readTVar t
-          writeTVar t $! UnorderedContainers.insert i c tv
+          writeTVar t $! UnorderedContainers.insert (iToK i) c tv
+    ,
+    bench "Unordered Containers" $
+      nf (foldr (\i -> UnorderedContainers.insert (iToK i) ()) UnorderedContainers.empty) [0..rows]
+    ,
+    bench "Containers" $
+      nf (foldr (\i -> Containers.insert (iToK i) ()) Containers.empty) [0..rows]
     ,
     bench "Hashtables" $ 
       do
-        t <- Hashtables.new :: IO (Hashtables.BasicHashTable Int ())
-        forM_ [0..rows] $ \i -> Hashtables.insert t i ()
-    ,
-    bench "Unordered Containers" $
-      nf (foldr (\k -> UnorderedContainers.insert k ()) UnorderedContainers.empty) [0..rows]
-    ,
-    bench "Containers" $
-      nf (foldr (\k -> Containers.insert k ()) Containers.empty) [0..rows]
+        t <- Hashtables.new :: IO (Hashtables.BasicHashTable String ())
+        forM_ [0..rows] $ \i -> Hashtables.insert t (iToK i) ()
   ]
 
 rows :: Int = 100000
+
+-- | Produce a pseudo-random key.
+iToK :: Int -> String
+iToK i = case Random.mkStdGen i of g -> take 7 $ Random.randomRs ('a', 'z') g
