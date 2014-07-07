dd813322f655d6d4a14ae177b5777ff988eb0303
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-07-07 15:34:55 +0400

    Add Multimap

--- library/STMContainers/Multimap.hs
+++ library/STMContainers/Multimap.hs
@@ -1,11 +1,10 @@
 module STMContainers.Multimap
 (
   Multimap,
-  Indexable,
+  Association,
   new,
   insert,
   delete,
-  focus,
   lookup,
   foldM,
   null,
@@ -19,51 +18,79 @@ import qualified STMContainers.Set as Set
 
 
 -- |
+-- A multimap, based on an STM-specialized hash array mapped trie.
+-- 
+-- Basically it's just a wrapper API around @'Map.Map' k ('Set.Set' v)@.
 newtype Multimap k v = Multimap (Map.Map k (Set.Set v))
 
 -- |
--- A standard constraint for keys.
-type Indexable a = (Eq a, Hashable a)
+-- A standard constraint for items.
+type Association k v = (Eq k, Hashable k, Eq v, Hashable v)
 
-lookup :: (Indexable k) => k -> Multimap k v -> STM (Maybe (Set.Set v))
-lookup k (Multimap m) = Map.lookup k m
+-- |
+-- Look up an item by a value and a key.
+{-# INLINE lookup #-}
+lookup :: (Association k v) => v -> k -> Multimap k v -> STM Bool
+lookup v k (Multimap m) = 
+  maybe (return False) (Set.lookup v) =<< Map.lookup k m
 
-insert :: (Indexable k, Indexable v) => k -> v -> Multimap k v -> STM ()
-insert k v = focus (Focus.insertM v) k v
-
-delete :: (Indexable k, Indexable v) => k -> v -> Multimap k v -> STM ()
-delete k v = focus Focus.deleteM k v
-
-focus :: (Indexable k, Indexable v) => Focus.StrategyM STM v r -> k -> v -> Multimap k v -> STM r
-focus s k v (Multimap m) = 
+-- |
+-- Insert an item.
+{-# INLINABLE insert #-}
+insert :: (Association k v) => v -> k -> Multimap k v -> STM ()
+insert v k (Multimap m) =
   Map.focus ms k m
   where
-    ms = \case
-      Nothing -> do
-        (r, d) <- s Nothing
-        case d of
-          Focus.Replace v' -> do
-            set <- Set.new
-            Set.insert v' set
-            return (r, Focus.Replace set)
-          _ -> return (r, Focus.Keep)
-      Just set -> do
-        r <- Set.focus s v set
-        Set.null set >>= \case
-          False -> return (r, Focus.Keep)
-          True -> return (r, Focus.Remove)
+    ms = 
+      \case 
+        Just s -> 
+          do
+            Set.insert v s
+            return ((), Focus.Keep)
+        Nothing ->
+          do
+            s <- Set.new
+            Set.insert v s
+            return ((), Focus.Replace s)
 
+-- |
+-- Delete an item by a value and a key.
+{-# INLINABLE delete #-}
+delete :: (Association k v) => v -> k -> Multimap k v -> STM ()
+delete v k (Multimap m) =
+  Map.focus ms k m
+  where
+    ms = 
+      \case 
+        Just s -> 
+          do
+            Set.delete v s
+            Set.null s >>= returnDecision . bool Focus.Keep Focus.Remove
+        Nothing ->
+          returnDecision Focus.Keep
+      where
+        returnDecision c = return ((), c)
+
+-- |
+-- Fold all the items.
+{-# INLINE foldM #-}
 foldM :: (a -> (k, v) -> STM a) -> a -> Multimap k v -> STM a
 foldM f a (Multimap m) = 
   Map.foldM f' a m
   where
-    f' a' (Map.Association k set) = 
+    f' a' (k, set) = 
       Set.foldM f'' a' set
       where
         f'' a'' v = f a'' (k, v)
 
+-- |
+-- Construct a new multimap.
+{-# INLINE new #-}
 new :: STM (Multimap k v)
 new = Multimap <$> Map.new
 
+-- |
+-- Check on being empty.
+{-# INLINE null #-}
 null :: Multimap k v -> STM Bool
 null (Multimap m) = Map.null m
