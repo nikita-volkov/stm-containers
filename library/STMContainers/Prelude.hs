d464c65f75aa3b765f64d38ce75da69e87130969
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-05-14 09:12:07 +0200

    Drop the error-reporting dependencies

--- library/STMContainers/Prelude.hs
+++ library/STMContainers/Prelude.hs
@@ -1,8 +1,6 @@
 module STMContainers.Prelude
 ( 
   module Exports,
-  bug,
-  bottom,
   traversePair,
 )
 where
@@ -10,10 +8,6 @@ where
 -- base
 -------------------------
 import BasePrelude as Exports
-
--- placeholders
--------------------------
-import Development.Placeholders as Exports
 
 -- hashable
 -------------------------
@@ -26,16 +20,6 @@ import Control.Monad.Trans.Class as Exports
 -- list-t
 -------------------------
 import ListT as Exports (ListT)
-
--- custom
--------------------------
-import qualified Debug.Trace.LocationTH
-
-bug = [e| $(Debug.Trace.LocationTH.failure) . (msg <>) |]
-  where
-    msg = "A \"stm-containers\" package bug: " :: String
-
-bottom = [e| $bug "Bottom evaluated" |]
 
 -- | A replacement for the missing 'Traverse' instance of pair in base < 4.7.
 traversePair :: Functor f => (a -> f b) -> (c, a) -> f (c, b)
